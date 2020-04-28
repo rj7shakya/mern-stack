cd543ac9744ca7c8791345977abd68302e53263f
rj7shakya <shakyarajad1@gmail.com>
2020-04-28 23:41:11 +0545

    contact kepper

---
+++ contact-keeper/routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+// route GET  api/contacts
+// desc  get all users contacts
+// access  private
+router.post("/", (req, res) => {
+  res.send("Get all contacts");
+});
+
+// route POST  api/contacts
+// desc  Add a new contact
+// access  private
+router.post("/", (req, res) => {
+  res.send("Add contact");
+});
+
+// route PUT  api/contacts/:id
+// desc  update contact
+// access  private
+router.post("/:id", (req, res) => {
+  res.send("Update contact");
+});
+
+// route DELETE  api/contacts
+// desc  Delete contact
+// access  private
+router.post("/:id", (req, res) => {
+  res.send("Delete contact");
+});
+
+module.exports = router;

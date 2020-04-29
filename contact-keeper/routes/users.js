7176b2b4ffd21d546a196f48c668afa5a2699f9a
rj7shakya <shakyarajad1@gmail.com>
2020-04-29 09:30:42 +0545

    contact keeper

--- contact-keeper/routes/users.js
+++ contact-keeper/routes/users.js
@@ -1,11 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const { check, validationResult } = require("express-validator/check");
 
 // route POST  api/users
 // desc  register a user
 // access  public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please include a valid email").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 ore more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).send({ msg: "User already exists" });
+      }
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -5,6 +5,9 @@ const connectDB = require("./config/db");
 //connect database
 connectDB();
 
+//Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });

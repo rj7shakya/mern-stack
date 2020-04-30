f56ff67e9f10967afee801ee1e082495eb31a294
rj7shakya <shakyarajad1@gmail.com>
2020-04-30 08:06:24 +0545

    react part

--- contact-keeper/client/src/components/contacts/ContactForm.js
+++ contact-keeper/client/src/components/contacts/ContactForm.js
@@ -1,6 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+  const { addContact, current, clearCurrent, updateContact } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal",
+      });
+    }
+  }, [contactContext, current]);
+
   const [contact, setContact] = useState({
     name: "",
     email: "",
@@ -12,9 +29,25 @@ const ContactForm = () => {
   const onChange = (e) =>
     setContact({ ...contact, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
+  };
+
+  const clearAll = () => {
+    clearCurrent();
+  };
+
   return (
-    <form>
-      <h2 className="text-primary">Add Contact</h2>
+    <form onSubmit={onSubmit}>
+      <h2 className="text-primary">
+        {current ? "Update Contact" : "Add Contact"}
+      </h2>
       <input
         type="text"
         placeholder="Name"
@@ -41,25 +74,31 @@ const ContactForm = () => {
         type="radio"
         name="type"
         value="personal"
-        checked={type === "Personal"}
+        checked={type === "personal"}
+        onChange={onChange}
       />
       Personal{" "}
       <input
         type="radio"
         name="type"
         value="professional"
-        checked={type === "Professional"}
+        checked={type === "professional"}
+        onChange={onChange}
       />
       Professional
       <div>
         <input
           type="submit"
-          value="Add Contact"
+          value={current ? "Update Contact" : "Add Contact"}
           className="btn btn-primary btn-block"
         />
+      </div>
+      <div>
+        <button className="btn btn-light btn-block" onClick={clearAll}>
+          Clear
+        </button>
       </div>
     </form>
   );
 };
-
 export default ContactForm;
--- contact-keeper/client/src/context/contact/ContactState.js
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuid } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 
@@ -38,28 +38,60 @@ const ContactState = (props) => {
         type: "professional",
       },
     ],
+    current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add contact
+  const addContact = (contact) => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // delete contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // set current contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // clear current contact
+  const clearCurrent = (contact) => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // update contact
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // filter contact
-
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
   // clear filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
       }}
     >
       {props.children}

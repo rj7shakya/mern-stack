4cfd88846163ed8ad543dd06c96a803247019aa7
rj7shakya <shakyarajad1@gmail.com>
2020-05-20 13:42:33 +0545

    node setup

--- not/blog site/frontend/config.js
+++
@@ -1,8 +0,0 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
-
-export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://seoblog.com"
-  : "http://localhost:8000";
-
-export const APP_NAME = publicRuntimeConfig.APP_NAME;

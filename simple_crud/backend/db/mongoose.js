c366656e1e29c5f4d4dea1da81d11abc45bf7735
rajad <rj7shakya@example.com>
2020-05-28 17:40:05 +0545

    backend crud done

--- simple_crud/backend/db/mongoose.js
+++ simple_crud/backend/db/mongoose.js
@@ -5,9 +5,7 @@ const mongodb = require('mongodb');
 mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology:true,
     useNewUrlParser: true,
-    findAndModify: true,
     useUnifiedTopology:true,
-    useCreateindex: true
 },(error,client)=>{
     if(error){
         return console.log('unable to connect to db');
